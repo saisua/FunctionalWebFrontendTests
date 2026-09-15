f90933b61dca6883bb86482a9efcccc899e4cd89
aus <aus@email.es>
2026-09-15 21:03:20 +0200

    Expanded reactivity to allow inner classes and access the current state machine state

--- fun_django_web/src/reactivity/reactive.py
+++ fun_django_web/src/reactivity/reactive.py
@@ -3,10 +3,12 @@ from js import document
 
 class Reactive:
 	_reactive_attrs: list[str] = ['if_', 'elif_', 'else_', 'get', 'set']
-	_reactive_values: dict[str, dict[str, list[object]]]
+	_reactive_values: dict[str, dict[str, list[object] | dict]]
 
 	def __init__(self) -> None:
 		self._reactive_values = dict()
+
+		reactive_inner_classes: set[type] = set()
 
 		reactive_elements = document.querySelectorAll(
 			f'[{"], [".join(self._reactive_attrs)}]'
@@ -16,20 +18,32 @@ class Reactive:
 				if element.hasAttribute(mod):
 					var = element.getAttribute(mod)
 
-					var_attrs = self._reactive_values.setdefault(
-						var,
-						dict()
-					)
+					obj = self
+					sub_rvals = self._reactive_values
 
-					var_attrs.setdefault(mod, list()).append(element)
+					var_path = var.split('.')
+					for subpath in var_path:
+						obj = getattr(obj, subpath)
 
-					self._check_mod(mod, element, getattr(self, var))
+						if isinstance(obj, type):
+							sub_rvals = sub_rvals.setdefault(f"__cls_{obj.__qualname__}", dict())
 
-		print("Reactive:", self._reactive_values)
+							obj._reactive_values = sub_rvals
 
-	def _check_mod(self, mod, el, value):
-		print(mod, el)
+							reactive_inner_classes.add(obj)
+						else:
+							var_attrs = sub_rvals.setdefault(
+								subpath,
+								dict()
+							)
 
+					var_attrs.setdefault(mod, list()).append(element)  # noqa: E501 # pyright: ignore[reportPossiblyUnboundVariable]
+
+					self._check_mod(mod, element, obj)
+		# print(f"Reactive: {self._reactive_values}")
+
+	@staticmethod
+	def _check_mod(mod, el, value):
 		if callable(value):
 			value = value()
 
@@ -44,13 +58,18 @@ class Reactive:
 				el.innerText = str(value)
 
 	def __setattr__(self, name, value):
-		print("Setattr", name, value)
-		if name in ("_reactive_values", "_reactive_attrs"):
+		if not name == '_current_state' and any(
+			name in base.__annotations__.keys()
+			for base in type(self).__bases__
+		):
+			print(f"Updated lib attr {name} to {value}")
 			return super().__setattr__(name, value)
 
 		if (modifiers := self._reactive_values.get(name)) is None:
+			print(f"Updated untracked var {name} to {value}")
 			return
 
+		print(f"Updated tracked var {name} to {value}")
 		for mod, elements in modifiers.items():
 			for el in elements:
 				self._check_mod(mod, el, value)
