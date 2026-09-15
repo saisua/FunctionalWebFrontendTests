e09d3d1d7aabd1a36c2d9b1187906fd7bcc6c156
aus <aus@email.es>
2026-09-15 21:04:38 +0200

    Moved a inner class from the mid of the parent class to the top of it

--- fun_django_web/src/state_machine/state_machine.py
+++ fun_django_web/src/state_machine/state_machine.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Self, Callable
 from dataclasses import dataclass
 
 try:
@@ -8,13 +8,61 @@ except ImportError:
 
 
 class StateMachine:
-	_current_state: str
-	__initial_state: str
+	@dataclass
+	class State:
+		name: str | int
+		initial: bool = False
+		final: bool = False
+
+		_before_fn: Callable | None = None
+		_on_fn: Callable | None = None
+		_after_fn: Callable | None = None
+
+		def to(self, other: Self, *, transition_type: str = 'call'):
+			return Transition(self, other, transition_type)
+
+		def to_itself(self):
+			return self.to(self)
+
+		def to_outside(self, out_url: str):
+			return self.to(out_url, transition_type='outside')
+
+		def __eq__(self, value: Self) -> bool:  # pyright: ignore[reportIncompatibleMethodOverride] # noqa: E501
+			if isinstance(value, StateMachine.State):
+				return value.name == self.name
+			return value == self.name
+
+		def __hash__(self) -> int:
+			return self.name.__hash__()
+
+		def is_current(self):
+			def _is_current(state, event) -> bool:
+				return state._current_state == self
+			return _is_current
+
+		def before(self, fn):
+			self._before_fn = fn
+			return fn
+
+		def on(self, fn):
+			self._on_fn = fn
+			return fn
+
+		def after(self, fn):
+			self._after_fn = fn
+			return fn
+
+		def __repr__(self) -> str:
+			return f"<{type(self).__name__} {self.name}>"
+
+	_current_state: str | int | State
+	__initial_state: str | int | State
 
 	def __init__(self) -> None:
 		try:
-			import pyodide
-			for attr in self.__annotations__.keys():
+			import pyodide  # TODO: Move to lazy import
+
+			for attr in type(self).__annotations__.keys():
 				val = getattr(self, attr)
 				if isinstance(val, Transition):
 					def _attr_transition(
@@ -31,23 +79,6 @@ class StateMachine:
 
 	def _reset_sm_state(self):
 		self._current_state = self.__initial_state
-
-	@dataclass
-	class State:
-		name: str
-		initial: bool = False
-		final: bool = False
-
-		def to(self, other: Union[str, 'State'], *, transition_type: str = 'call'):
-			if isinstance(other, StateMachine.State):
-				other = other.name
-			return Transition(self.name, other, transition_type)
-
-		def to_itself(self):
-			return self.to(self)
-
-		def to_outside(self, out_url: str):
-			return self.to(out_url, transition_type='outside')
 
 	def __init_subclass__(cls) -> None:
 		found_state: bool = False
@@ -67,8 +98,8 @@ class StateMachine:
 		if found_state:
 			if initial_state is None:
 				raise ValueError("It is required to define a initial state")
-			cls.__initial_state = initial_state.name
-			cls._current_state = initial_state.name
+			cls.__initial_state = initial_state
+			cls._current_state = initial_state
 
 
 State = StateMachine.State
