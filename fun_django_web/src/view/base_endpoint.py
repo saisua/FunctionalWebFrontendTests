2e38584857c0e252ca5bbddc20a02fc4a4c3380d
aus <aus@email.es>
2026-09-15 21:08:29 +0200

    Improved script dependency injection, and the generated script is now a string instead of a callable to allow caching it

--- fun_django_web/src/view/base_endpoint.py
+++ fun_django_web/src/view/base_endpoint.py
@@ -1,25 +1,52 @@
-from typing import Callable
 from pathlib import Path
+from textwrap import dedent
 
-from django.http import HttpResponse
 from django.conf import settings
-from django.views.decorators.csrf import ensure_csrf_cookie
 
 from fun_django_web.src.page.page import Page
 
 
-STATIC_URL = Path(getattr(
-	settings,
-	'STATIC_URL',
-	'static'
-).strip('/'))
-ABS_STATIC_URL = settings.STATIC_DIR.parent
+STATIC_URL = Path(settings.STATIC_URL)
+RESOURCES_DIR = Path(settings.BASE_DIR) / "fun_django_web" / "generated"
+RESOURCES_DIR.mkdir(parents=True, exist_ok=True)
+
+resources_gitignore = RESOURCES_DIR / '.gitignore'
+if not resources_gitignore.exists():
+	resources_gitignore.write_text('*')
+
+PY_SCRIPTS = [
+	STATIC_URL / "utils" / "classproperty.py",
+	STATIC_URL / "utils" / "docstr.py",
+	STATIC_URL / "utils" / "partial_format.py",
+	STATIC_URL / "requests" / "make_requests.py",
+	STATIC_URL / "requests" / "request.py",
+	STATIC_URL / "notifications" / "notifications.py",
+	STATIC_URL / "reactivity" / "reactive.py",
+	STATIC_URL / "state_machine" / "transition.py",
+	STATIC_URL / "state_machine" / "state_machine.py",
+	STATIC_URL / "declarative" / "fact.py",
+	STATIC_URL / "declarative" / "functions" / "update_class_ref.py",
+	STATIC_URL / "declarative" / "functions" / "immutable_class_ref.py",
+	STATIC_URL / "declarative" / "functions" / "callback.py",
+	STATIC_URL / "declarative" / "functions" / "decorators.py",
+	STATIC_URL / "declarative" / "functions" / "io.py",
+	STATIC_URL / "declarative" / "functions" / "pure.py",
+	STATIC_URL / "declarative" / "events" / "callback.py",
+	STATIC_URL / "declarative" / "events" / "io.py",
+	STATIC_URL / "declarative" / "events" / "timer.py",
+]
+
+# TODO
+PY_LIBS = [
+	# 'micropip'
+	'requests'
+]
 
 
-def gen_base_endpoint(
+def gen_base_index(
 	cls: type,
 	built_resources,
-) -> Callable[[object], HttpResponse]:
+) -> str:
 	print("Generating base endpoint for", cls)
 
 	page = Page()
@@ -36,71 +63,51 @@ def gen_base_endpoint(
 		page.stag(
 			"script",
 			type="module",
-			src=str(STATIC_URL / "pyscript" / "package" / "index.js")
+			src=str(STATIC_URL / "libs" / "pyodide" / "package" / "pyodide.js")
 		)
+
+		page.stag(
+			"script",
+			type="module",
+			src=str(STATIC_URL / "libs" / "pyscript" / "package" / "index.js")
+		)
+
 		page.tag(
 			"py-config",
 			style='display: none',
-		).text(
-			"interpreter='" +
-			str(STATIC_URL / "pyodide" / "package" / "pyodide.mjs") +
-			"'"
-		)
+		).text(dedent(
+			f"""
+			interpreter='{str(STATIC_URL / "libs" / "pyodide" / "package" / "pyodide.mjs")}'
+			"""
+		))
 
-		page.stag(
-			"script",
-			type="py",
-			src=str(
-				STATIC_URL /
-				"make_requests.py"
+		for py_script_path in PY_SCRIPTS:
+			page.stag(
+				"script",
+				type="py",
+				src=str(py_script_path)
 			)
-		)
-
-		page.stag(
-			"script",
-			type="py",
-			src=str(
-				STATIC_URL /
-				"notifications.py"
-			)
-		)
-
-		page.stag(
-			"script",
-			type="py",
-			src=str(
-				STATIC_URL / "transition.py"
-			)
-		)
-
-		page.stag(
-			"script",
-			type="py",
-			src=str(
-				STATIC_URL / "reactive.py"
-			)
-		)
-
-		page.stag(
-			"script",
-			type="py",
-			src=str(
-				STATIC_URL / "state_machine.py"
-			)
-		)
 
 		if built_resources.back_methods:
 			page.stag(
 				"script",
 				type="py",
-				src=str(built_resources.back_methods.relative_to(ABS_STATIC_URL))
+				src=str(
+					STATIC_URL
+					/ "generated_pages"
+					/ built_resources.back_methods.relative_to(RESOURCES_DIR)
+				)
 			)
 
 		if built_resources.page_script:
 			page.stag(
 				"script",
 				type="py",
-				src=str(built_resources.page_script.relative_to(ABS_STATIC_URL))
+				src=str(
+					STATIC_URL
+					/ "generated_pages"
+					/ built_resources.page_script.relative_to(RESOURCES_DIR)
+				)
 			)
 		else:
 			print(
@@ -111,7 +118,11 @@ def gen_base_endpoint(
 			page.stag(
 				"script",
 				type="py",
-				src=str(built_resources.front_view_class.relative_to(ABS_STATIC_URL))
+				src=str(
+					STATIC_URL
+					/ "generated_pages"
+					/ built_resources.front_view_class.relative_to(RESOURCES_DIR)
+				)
 			)
 
 		if built_resources.stylesheets:
@@ -119,23 +130,21 @@ def gen_base_endpoint(
 			if not isinstance(stylesheets, (list, tuple, set)):
 				stylesheets = [stylesheets]
 
+			stylesheet: Path
 			for stylesheet in stylesheets:
 				page.stag(
 					"link",
 					rel="stylesheet",
-					href=str(stylesheet.relative_to(ABS_STATIC_URL))
+					href=str(
+						STATIC_URL
+						/ "generated_pages"
+						/ stylesheet.relative_to(RESOURCES_DIR)
+					)
 				)
 
-	page_src: bytes = page.render().encode()
-	page_response = HttpResponse(page_src, status=200)
-	del cls, built_resources, page, page_src
+	page_src: str = page.render()
+	(RESOURCES_DIR / cls._endpoint / "index.html").write_text(
+		page_src
+	)
 
-	@ensure_csrf_cookie
-	def _base_endpoint_wrapper(
-		self,
-		*,
-		page_response: HttpResponse = page_response,
-	) -> HttpResponse:
-		return page_response
-
-	return _base_endpoint_wrapper
+	return page_src
