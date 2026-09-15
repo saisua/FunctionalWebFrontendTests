3e78d4b1c24b92bd504ba70d1e3004de36f3f5ce
aus <aus@email.es>
2026-09-15 21:00:26 +0200

    Improved typing and updated a few paths

--- fun_django_web/pages/base/base.py
+++ fun_django_web/pages/base/base.py
@@ -1,3 +1,5 @@
+from typing import cast
+
 from fun_django_web.src.page.page import Page
 
 from fun_django_web.components.base.header import render as render_header
@@ -6,24 +8,28 @@ from fun_django_web.components.base.footer import render as render_footer
 from fun_django_web.components.loading.loading import render as render_loading
 
 
-def render(*args, **kwargs) -> Page:
+class PageWithContent(Page):
+	content: Page._LazyTag
+
+
+def render(*args, **kwargs) -> PageWithContent:
 	page = Page()
 
 	page._template_prefixes.append("{% load static %}")
 	page.add_pyscript_file(
-		'py/state_machine.py',
+		'state_machine/state_machine.py',
 		static=True,
 		target_path='state_machine.py',
 		run=False,
 	)
 	page.add_pyscript_file(
-		'py/workflows.py',
+		'workflows/workflows.py',
 		static=True,
 		target_path='workflows.py',
 		run=False,
 	)
 	page.add_pyscript_file(
-		'py/notifications.py',
+		'notifications/notifications.py',
 		static=True,
 		target_path='notifications.py',
 		run=False,
@@ -44,11 +50,19 @@ def render(*args, **kwargs) -> Page:
 		)
 		page.tag("title").text("MyApp")
 
-		page.stag("link", rel="icon", href="{% static 'img/favicon.ico' %}")
+		page.stag(
+			"link",
+			rel="icon",
+			href="{% static 'admin/img/icon-debug-dark.svg' %}"
+		)
 
-		page.stag("link", rel="stylesheet", href="{% static 'css/base.css' %}")
+		page.stag("link", rel="stylesheet", href="{% static 'admin/css/base.css' %}")
 
-		page.stag("script", type="module", src="{% static 'pyscript/package/dist/core.js' %}")
+		page.stag(
+			"script",
+			type="module",
+			src="{% static 'libs/pyscript/package/dist/core.js' %}"
+		)
 
 	with page.body:
 		with page.tag("div", klass="app-wrap"):
@@ -74,4 +88,4 @@ def render(*args, **kwargs) -> Page:
 	#
 	render_loading(page)
 
-	return page
+	return cast(PageWithContent, page)
