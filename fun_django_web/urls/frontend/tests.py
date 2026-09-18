96d6501672d3a4529d15713d37d6ea7a78fefc84
aus <aus@email.es>
2026-09-18 14:50:41 +0200

    Updated to disable previous non working versions of page

--- fun_django_web/urls/frontend/tests.py
+++ fun_django_web/urls/frontend/tests.py
@@ -1,9 +1,22 @@
 from django.urls import path
 
+
+class fake_view:
+	@staticmethod
+	def get_urls(): return tuple()
+
+
 from fun_django_web.pages.test import render as test_render
 from fun_django_web.pages.test2 import render as test2_render
-from fun_django_web.pages.test3 import view as test3_view
-from fun_django_web.pages.test4 import view as test4_view
+try:
+	from fun_django_web.pages.test3 import view as test3_view
+except Exception:
+	test3_view = fake_view
+try:
+	from fun_django_web.pages.test4 import view as test4_view
+except Exception:
+	test4_view = fake_view
+from fun_django_web.pages.test5 import view as test5_view
 
 
 urlpatterns = [
@@ -11,4 +24,5 @@ urlpatterns = [
 	path("test2/", test2_render, name="test2"),
 	*test3_view.get_urls(),
 	*test4_view.get_urls(),
+	*test5_view.get_urls(),
 ]
